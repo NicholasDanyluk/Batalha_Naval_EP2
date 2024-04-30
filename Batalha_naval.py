23c87cdff66b4c81b11ea780dd0eee0ab444919f
NicholasDanyluk <nicholasdanyluk@hotmail.com>
2024-04-30 14:36:14 -0300

    Mensagens iniciais e definições

--- Batalha_naval.py
+++ Batalha_naval.py
@@ -10,6 +10,66 @@
 # White: \u001b[37m
 # Reset: \u001b[0m
 
+ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+CORES = {
+    'reset': '\u001b[0m',
+    'red': '\u001b[31m',
+    'black': '\u001b[30m',
+    'green': '\u001b[32m',
+    'yellow': '\u001b[33m',
+    'blue': '\u001b[34m',
+    'magenta': '\u001b[35m',
+    'cyan': '\u001b[36m',
+    'white': '\u001b[37m'
+}
+
+CONFIGURACAO = {
+    'destroyer': 3,
+    'porta-avioes': 5,
+    'submarino': 2,
+    'torpedeiro': 3,
+    'cruzador': 2,
+    'couracado': 4
+}
+
+PAISES =  {
+    'Brasil': {
+        'cruzador': 1,
+        'torpedeiro': 2,
+        'destroyer': 1,
+        'couracado': 1,
+        'porta-avioes': 1
+    }, 
+    'França': {
+        'cruzador': 3, 
+        'porta-avioes': 1, 
+        'destroyer': 1, 
+        'submarino': 1, 
+        'couracado': 1
+    },
+    'Austrália': {
+        'couracado': 1,
+        'cruzador': 3, 
+        'submarino': 1,
+        'porta-avioes': 1, 
+        'torpedeiro': 1
+    },
+    'Rússia': {
+        'cruzador': 1,
+        'porta-avioes': 1,
+        'couracado': 2,
+        'destroyer': 1,
+        'submarino': 1
+    },
+    'Japão': {
+        'torpedeiro': 2,
+        'cruzador': 1,
+        'destroyer': 2,
+        'couracado': 1,
+        'submarino': 1
+    }
+}
 
 #********************************#  FUNÇÕES  #********************************#
 
@@ -105,3 +165,64 @@ def aloca_navios(mapa,tamanhos):
 
 #**********************************#  JOGO  #*********************************#
 
+import random
+
+### define o pais do computador ###
+paises = ['Brasil','França','Austrália','Rússia','Japão']
+p = ['1','2','3','4','5']
+pais_pc = random.choice(paises)
+
+### printa as mensagens iniciais ###
+welcome = ''' ===================================== 
+|                                     |
+| Bem-vindo ao INSPER - Batalha Naval |
+|                                     |
+ =======   xxxxxxxxxxxxxxxxx   ======= '''
+
+aviso = f'Iniciando o jogo!\n\nComputador está alocando os navios de guerra do país {pais_pc}...\nComputador já está em posição de batalha!'
+
+config = '''
+1: Brasil
+   1 cruzador
+   2 torpedeiro
+   1 destroyer
+   1 couracado
+   1 porta-avioes
+
+2: França
+   3 cruzador
+   1 porta-avioes
+   1 destroyer
+   1 submarino
+   1 couracado
+
+3: Austrália
+   1 couracado
+   3 cruzador
+   1 submarino
+   1 porta-avioes
+   1 torpedeiro
+
+4: Rússia
+   1 cruzador
+   1 porta-avioes
+   2 couracado
+   1 destroyer
+   1 submarino
+
+5: Japão
+   2 torpedeiro
+   1 cruzador
+   2 destroyer
+   1 couracado
+   1 submarino
+'''
+
+print(f'{welcome}\n\n{aviso}\n{config}')
+
+### define o pais do jogador ###
+jogador = input('Qual o número da nação da sua frota? ')
+while jogador not in p:
+    print('Opção inválida')
+    jogador = input('Qual o número da nação da sua frota? ')
+print(f'Você escolheu a nação {paises[int(jogador)-1]}\nAgora é sua vez de alocar seus navios de guerra!')
